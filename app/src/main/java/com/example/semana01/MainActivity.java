f32277ffab7ef34b464609ed1da1b405172bb2f4
DanielaR123 <100729036+DanielaR123@users.noreply.github.com>
2022-09-09 17:22:15 -0500

    first commit

---
+++ app/src/main/java/com/example/semana01/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.semana01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btncalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btncalcular=(Button)findViewById(R.id.btncalcular);
+
+        btncalcular.setOnClickListener(this);
+    }
+
+    public void Calcular() {
+
+        Toast.makeText(getApplicationContext(), "El producto es: ",Toast.LENGTH_LONG).show();
+
+    }
+    @Override
+    public void onClick(View v)
+    {
+        if(v==btncalcular)
+        {
+            Calcular();
+        }
+    }
+}
